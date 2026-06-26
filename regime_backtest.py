8603e7c2337e2f55e1d0e1ef5ecddfd046de237c
viraltrend <catchitearly.viraltrend@gmail.com>
2026-06-26 13:56:50 +0530

    Update regime_backtest.py

--- regime_backtest.py
+++ regime_backtest.py
@@ -52,7 +52,7 @@ TOP_N     = 3
 COST_PCT  = 0.001
 INITIAL   = 1_000_000
 
-FETCH_START  = "2015-01-01"
+FETCH_START  = "2019-01-01"
 BT_END       = "2024-12-31"   # backtest period end
 FT_START     = "2025-01-01"   # forward test start
 END          = date.today().strftime("%Y-%m-%d")
